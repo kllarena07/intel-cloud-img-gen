4480156a21f4abda9e6436879eb6a1e4279fbb0b
Kieran Llarena <baymax709@gmail.com>
2024-04-30 12:25:57 -0400

    test(backend/testing): webhook test

--- backend/testing/test_server.py
+++ backend/testing/test_server.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI, status
+import asyncio
 
 app = FastAPI()
+
+async def simulate_background_task():
+    await asyncio.sleep(5)
+    return {"result": "Task completed!"}
 
 @app.get("/task")
 async def task():
