b4aa84a48e1c1e51e7835b4b450ad899797608f9
Kieran Llarena <baymax709@gmail.com>
2024-04-30 13:09:53 -0400

    chore(backend/testing): copy and paste from fastapi documentation

--- backend/testing/test_server.py
+++ backend/testing/test_server.py
@@ -1,14 +1,14 @@
-from fastapi import FastAPI, status
-import asyncio
+from fastapi import BackgroundTasks, FastAPI
 
 app = FastAPI()
 
-async def simulate_background_task():
-    await asyncio.sleep(5)
-    return {"result": "Task completed!"}
+def write_notification(email: str, message=""):
+    with open("log.txt", mode="w") as email_file:
+        content = f"notification for {email}: {message}"
+        email_file.write(content)
 
-@app.get("/task")
-async def task():
-    return {
-      "message": "Task accepted"
-    }, status.HTTP_202_ACCEPTED
+
+@app.post("/send-notification/{email}")
+async def send_notification(email: str, background_tasks: BackgroundTasks):
+    background_tasks.add_task(write_notification, email, message="some notification")
+    return {"message": "Notification sent in the background"}
