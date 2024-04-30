860a42e3775fb61eeff5fe2ab5bfe36911b039f2
Kieran Llarena <baymax709@gmail.com>
2024-04-30 12:25:09 -0400

    feat(backend/testing): task endpoint

--- backend/testing/test_server.py
+++ backend/testing/test_server.py
@@ -1,3 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 
 app = FastAPI()
+
+@app.get("/task")
+async def task():
+    return {
+      "message": "Task accepted"
+    }, status.HTTP_202_ACCEPTED
